af9582a8c79355d02695ad808bd647772d2cd87f
kingjnr4 <onyiboix@gmail.com>
2022-07-17 23:41:56 +0100

    created admin model

---
+++ src/models/admin.model.ts
@@ -0,0 +1,43 @@
+import { model, Schema, Document } from "mongoose";
+import { IAdmin } from "../interfaces/user.interface";
+import { logger } from "../utils/logger";
+import { compare, hashPassword } from "../utils/utils";
+
+const adminSchema: Schema = new Schema<IAdmin>({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+   username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+userSchema.pre("save", async function (next) {
+  try {
+    const user = this;
+    const hash = hashPassword(user.password);
+    user.password = hash;
+    next();
+  } catch (e) {
+    logger.error(e);
+    next(e);
+  }
+});
+userSchema.methods.isValidPassword = async function (password: string){
+  try {
+      let user = this;
+    return await compare(password, user.password);
+  } catch (e) {
+    logger.error(e);
+  }
+};
+
+const adminModel = model<Document & IAdmin>("admins", adminSchema);
+export default adminModel;
