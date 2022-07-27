faad9182399bdd1d0416de6db43efb1e264a415b
kingjnr4 <onyiboix@gmail.com>
2022-07-27 12:52:45 +0100

    added admin to server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import App from './app';
 import {Cron} from './cron';
+import AdminRoute from './routes/admin.route';
 import IndexRoute from './routes/index.route';
 import SubRoute from './routes/subscription.route';
 import UserRoute from './routes/user.route';
 
-const app = new App([new IndexRoute(), new UserRoute(), new SubRoute()]);
+const app = new App([new IndexRoute(), new UserRoute(), new SubRoute(),new AdminRoute()]);
 // Cron.start('0 * * * * *', function jobYouNeedToExecute() {
 //   console.log(new Date().toLocaleString());
 // });
