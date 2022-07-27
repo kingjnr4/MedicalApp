47b0666ff854f551166137b7858d83dc76c170b1
kingjnr4 <onyiboix@gmail.com>
2022-07-27 16:04:02 +0100

    added plan route

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import App from './app';
 import {Cron} from './cron';
 import AdminRoute from './routes/admin.route';
 import IndexRoute from './routes/index.route';
+import PlanRoute from './routes/plan.route';
 import SettingsRoute from './routes/settings.route';
 import SubRoute from './routes/subscription.route';
 import UserRoute from './routes/user.route';
@@ -12,6 +13,7 @@ const app = new App([
   new SubRoute(),
   new AdminRoute(),
   new SettingsRoute(),
+  new PlanRoute(),
 ]);
 // Cron.start('0 * * * * *', function jobYouNeedToExecute() {
 //   console.log(new Date().toLocaleString());
