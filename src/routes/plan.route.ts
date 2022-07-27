412b21c9d0f3ebc4dfd292b284b385642e2852f9
kingjnr4 <onyiboix@gmail.com>
2022-07-27 16:16:15 +0100

    created plan gaurd

--- src/routes/plan.route.ts
+++ src/routes/plan.route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import PlanController from "../controllers/plan.controller";
 import { CreatePlanDto, DeletePlanDto, UpdatePlanDto } from "../dtos/plan.dto";
+import { AdminGuard } from "../guards/admin.guard";
 import { AuthGuard } from "../guards/auth.guard";
 import { IRoute } from "../interfaces/routes.interfaces";
 import validationMiddleware from "../middlewares/validation.middleware";
@@ -18,18 +19,20 @@ class PlanRoute implements IRoute {
     this.router.post(
       `${this.path}/create`,
       validationMiddleware(CreatePlanDto, "body", "fields"),
-      AuthGuard.createInstance,
+      AdminGuard.createInstance,
       this.controller.create
     );
     this.router.delete(
       `${this.path}/delete`,
-      validationMiddleware(DeletePlanDto, "body", "key"),
-      this.controller.delete
+      validationMiddleware(DeletePlanDto, 'body', 'key'),
+      AdminGuard.createInstance,
+      this.controller.delete,
     );
     this.router.post(
       `${this.path}/update`,
-      validationMiddleware(UpdatePlanDto, "body", "fields"),
-      this.controller.update
+      validationMiddleware(UpdatePlanDto, 'body', 'fields'),
+      AdminGuard.createInstance,
+      this.controller.update,
     );
   }
 }
