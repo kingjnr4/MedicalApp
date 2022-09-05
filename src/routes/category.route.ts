070cceb17f040e49526c94e184438eed84a97c7b
kingjnr4 <onyiboix@gmail.com>
2022-09-05 14:58:56 +0100

    get all cats added

--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -1,23 +1,33 @@
-import { Router } from "express";
-import CategoryController from "../controllers/category.controller";
-import { CreateCatDto } from "../dtos/category.dto";
-import { AdminGuard } from "../guards/admin.guard";
-import { EmptyJwtGuard } from "../guards/jwtempty.guard";
-import { IRoute } from "../interfaces/routes.interfaces";
-import validationMiddleware from "../middlewares/validation.middleware";
+import {Router} from 'express';
+import CategoryController from '../controllers/category.controller';
+import {CreateCatDto} from '../dtos/category.dto';
+import {AdminGuard} from '../guards/admin.guard';
+import {EmptyJwtGuard} from '../guards/jwtempty.guard';
+import {IRoute} from '../interfaces/routes.interfaces';
+import validationMiddleware from '../middlewares/validation.middleware';
 
-class CategoryRoute implements IRoute{
-    public path = "/categories";
-    public router =  Router();
-    private controller = new CategoryController ()
-    constructor (){
-        this.initializeRoutes()
-    }
-    private initializeRoutes(){
-        this.router.post(`${this.path}\create`,EmptyJwtGuard.check,AdminGuard.createInstance,validationMiddleware(CreateCatDto,'body','fields'),this.controller.create)
-
-
-    }
+class CategoryRoute implements IRoute {
+  public path = '/categories';
+  public router = Router();
+  private controller = new CategoryController();
+  constructor() {
+    this.initializeRoutes();
+  }
+  private initializeRoutes() {
+    this.router.post(
+      `${this.path}\create`,
+      EmptyJwtGuard.check,
+      AdminGuard.createInstance,
+      validationMiddleware(CreateCatDto, 'body', 'fields'),
+      this.controller.create,
+    );
+    this.router.get(
+      `${this.path}\get`,
+      EmptyJwtGuard.check,
+      AdminGuard.createInstance,
+      this.controller.getAll,
+    );
+  }
 }
 
-export default CategoryRoute
+export default CategoryRoute;
