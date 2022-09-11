85148d49e28fafba91848302b4b7ceeaf3467b9d
kingjnr4 <onyiboix@gmail.com>
2022-09-11 21:54:25 +0100

    model update

--- src/models/antibioticguide.model.ts
+++ src/models/antibioticguide.model.ts
@@ -1,7 +1,15 @@
 import {Document, model, Schema} from 'mongoose';
 import {IAntibioticsGuide} from '../interfaces/diagnosis.interface';
 
-const antibioticSchema: Schema = new Schema<IAntibioticsGuide>({});
+const antibioticSchema: Schema = new Schema<IAntibioticsGuide>({
+  name: {
+    type: String,
+    required: true,
+  },
+  category: {type: String, required: true},
+  withPenicillin: {type: String, required: true},
+  withoutPenicillin: {type: String, required: true},
+});
 
 const antibioticModel = model<IAntibioticsGuide & Document>(
   'antibioticsGuide',
--- src/models/clinicalguide.model.ts
+++ src/models/clinicalguide.model.ts
@@ -1,7 +1,11 @@
 import {Document, model, Schema} from 'mongoose';
 import {IClinicalGuide} from '../interfaces/diagnosis.interface';
 
-const clinicalSchema: Schema = new Schema<IClinicalGuide>({});
+const clinicalSchema: Schema = new Schema<IClinicalGuide>({
+  category: {type: String, required: true},
+  disease: {type: String, required: true},
+  guide: {type: String, required: true},
+});
 const clinicalModel = model<IClinicalGuide & Document>(
   'clinicalGuide',
   clinicalSchema,
